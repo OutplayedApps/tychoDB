6d21fd070a88f49c3e793383f14c0867e8810ee4
Ashwin Ramaswami <aramaswamis@gmail.com>
2017-11-20 16:40:07 -0800

    some progress

--- parse-txt.py
+++ parse-txt.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import os
 import re
 import json
@@ -18,9 +19,15 @@ categoryList = {
 };
 
 searchString = (
-    r"(?P<questionNum>\d\d)[\.\)].*(?P<category>[A-Z ]+)"
-    r"(?i)(Short Answer|Multiple Choice) (?P<tossupQ>[\S\s]*(?=ANSWER))"
-    r"ANSWER\: (?P<tossupA>[\S\s]*)\n"
+    r"(TOSS\-UP|TOSSUP|TOSS UP)\s*"
+    r"(?P<questionNum>\d{1,2})[\.\)]\s*(?P<category>[A-Z ]+)\s*"
+    r"(?i)(Short Answer|Multiple Choice)\s*(?P<tossupQ>[\S\s]*)"
+    r"ANSWER\:(?P<tossupA>[\w ]*)"
+
+    r"\s*BONUS\s*"
+    r"(?P<questionNumBonus>\d{1,2})[\.\)]\s*(?P<categoryBonus>[A-Z ]+)\s*"
+    r"(?i)(Short Answer|Multiple Choice)\s*(?P<bonusQ>[\S\s]*)"
+    r"ANSWER\:(?P<bonusA>[\w\)\. ]*)\s*"
 )
 
 replaceString = r"""
@@ -29,14 +36,13 @@ replaceString = r"""
     "questionNum": {questionNum},
     "tossupQ": "{tossupQ}",
     "tossupA": "{tossupA}",
-    
-"""
-a="""
-"tossupA": "{tossupA}",
- "bonusQ": {bonusQ},
-    "bonusA": {bonusA}
+    "bonusQ": "{bonusQ}",
+    "bonusA": "{bonusA}"
 }},
 """
+
+def format(string):
+    return string.strip().encode("string_escape")
 
 def getCategoryId(category):
 
@@ -49,19 +55,79 @@ def getCategoryId(category):
         return categoryList["OTHER"]
 
 def parse(text):
+    text = """
+
+
+TOSS UP
+
+
+
+1. MATH  Short Answer  Pablo walks 4 miles north, 6 miles east, and then 2 miles north again. In simplest form, how many miles is he from his starting point?
+
+
+
+ANSWER: 6
+
+
+
+BONUS
+
+
+
+1. MATH  Short Answer  Evaluate the limit as x approaches infinity of x times the quantity negative 1 plus e to the 1 over x.
+
+
+
+ANSWER: 1
+
+
+
+TOSS UP
+
+
+
+2. CHEMISTRY  Multiple Choice  Which of the following is NOT a characteristic of amines?
+
+
+
+W) A fully protonated amine is called an ammonium ion
+
+X) Amines can function as Brønsted bases
+
+Y) The VSEPR geometry of the nitrogen atom is trigonal planar
+
+Z) Amines can be a hydrogen bond acceptor
+
+
+
+ANSWER: Y) The VSEPR geometry of the nitrogen atom is trigonal planar
+
+
+
+BONUS
+
+
+
+2. CHEMISTRY  Multiple Choice  Of the following elements in their monatomic gaseous states, which has the lowest electron affinity?
+
+
+
+W) BoronX) CarbonY) NitrogenZ) OxygenANSWER: Y) NITROGEN
+
+
+    """
     jsonstring = re.sub(
         searchString,
         lambda m: replaceString.format(
             categoryId=getCategoryId(m.group('category')),
-            questionNum = m.group('questionNum'),
-            tossupQ=m.group('tossupQ').strip(),
-            tossupA=m.group('tossupA').strip(),
-            #bonusQ=m.group('bonusQ'),
-            #bonusA=m.group('bonusA'),
+            questionNum = format(m.group('questionNum')),
+            tossupQ=format(m.group('tossupQ').strip()),
+            tossupA=format(m.group('tossupA').strip()),
+            bonusQ=format(m.group('bonusQ').strip()),
+            bonusA=format(m.group('bonusA'))
             ),
         text
         )
-    print jsonstring
     return jsonstring
     #return json.loads("[" + jsonstring + "]")
 
@@ -71,7 +137,6 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 input_full_path = dir_path + "\\" + input_folder + "\\"
 
 for fn in os.listdir(input_full_path):
-    print fn
     with open(input_full_path + fn) as f:
         text = f.read()
         text = parse(text)
