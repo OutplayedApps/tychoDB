69f951d8f70530844891fe785f5215f9e2e586ba
Ashwin Ramaswami <aramaswamis@gmail.com>
2017-11-22 14:42:58 -0800

    ready to import json from parsed packets now

--- mongo/importfromjson.py
+++ mongo/importfromjson.py
@@ -1,17 +1,25 @@
 import json
 from pymongo import MongoClient
 import urllib
+from datetime import datetime
+
 user = 'tycho'
 pwd = 'tych0@@@'
 user = urllib.quote_plus(user)
 pwd = urllib.quote_plus(pwd)
-#client = MongoClient('mongodb://'+user+':'+pwd+'@ds113566.mlab.com:13566/tycho')
+client = MongoClient('mongodb://'+user+':'+pwd+'@ds113566.mlab.com:13566/tycho')
 questions = client.tycho.questions
 
-with open('data.json') as json_data:
+with open('import/data.json') as json_data:
     d = json.load(json_data)
     for entry in d:
     	if (hasattr(entry, "questionNum")):
     		entry.questionNum = int(entry.questionNum)
+        entry['vendorNum'] = 'SAMPLE-RR-HS'
+    	entry['setNum'] = 1
+        entry['packetNum'] = 1
+        entry['date_modified'] = datetime.now().isoformat()
+        entry['date_created'] = datetime.now().isoformat()
+    # print str(d)
     questions.insert_many(d)
 print "done"
