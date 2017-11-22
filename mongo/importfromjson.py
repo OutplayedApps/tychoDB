f1070f27ade659a0097d0e6158f1161040f7e572
Ashwin Ramaswami <aramaswamis@gmail.com>
2017-11-22 15:18:08 -0800

    add in first batch of questions + their metadata

--- mongo/importfromjson.py
+++ mongo/importfromjson.py
@@ -7,7 +7,7 @@ user = 'tycho'
 pwd = 'tych0@@@'
 user = urllib.quote_plus(user)
 pwd = urllib.quote_plus(pwd)
-client = MongoClient('mongodb://'+user+':'+pwd+'@ds113566.mlab.com:13566/tycho')
+#client = MongoClient('mongodb://'+user+':'+pwd+'@ds113566.mlab.com:13566/tycho')
 questions = client.tycho.questions
 
 with open('import/data.json') as json_data:
@@ -15,9 +15,9 @@ with open('import/data.json') as json_data:
     for entry in d:
     	if (hasattr(entry, "questionNum")):
     		entry.questionNum = int(entry.questionNum)
-        entry['vendorNum'] = 'SAMPLE-RR-HS'
+        entry['vendorNum'] = 'SAMPLE-DE-HS'
     	entry['setNum'] = 1
-        entry['packetNum'] = 1
+        entry['packetNum'] = 2
         entry['date_modified'] = datetime.now().isoformat()
         entry['date_created'] = datetime.now().isoformat()
     print str(d)
